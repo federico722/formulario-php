d4e7a8c204387676d7285bb8815f6bcbed2890b4
Federico Pelaez Calderon <ADESARROLLO@CLUBCAMPESTRE.NET>
2025-01-20 16:56:04 -0500

    agrego validaciones

--- php/enviar_formulario.php
+++ php/enviar_formulario.php
@@ -59,59 +59,17 @@ try {
     // Motivación
     $motivacion_uso_club = $_POST['motivacion_uso_club'];
 
-    // Procesar los datos (por ejemplo, guardarlos en la base de datos)
-    echo "<h3>Datos recibidos:</h3>";
-    echo "Modalidad: " . $modalidad . "<br>";
-    echo "Número de derecho: " . $numero_derecho . "<br>";
-    echo "Nombre y Apellidos: " . $nombre_apellido_titular . "<br>";
-    echo "Tipo de identificación: " . $tipo_identificacion . "<br>";
-    echo "Número de identificación: " . $numero_identificacion . "<br>";
-    echo "Sexo: " . ($genero == 'F' ? 'Femenino' : 'Masculino') . "<br>";
+    // Validaciones para verificar si las variables están definidas
+    if (!isset($modalidad, $numero_derecho, $nombre_apellido_titular, $tipo_identificacion, $numero_identificacion, $genero, 
+    $frecuencia_usted, $frecuencia_beneficiarios, $gastronomicas, $culturales, $deportivas, $sociales, $text_cual, $otra_cual, 
+    $app_socios, $whatsapp, $mail, $revista_somos, $instagram, $facebook, $habladores, $mensaje_texto, $satisfaccion_general, 
+    $deportivas_satisfaccion, $gastronomicas_satisfaccion, $culturales_satisfaccion, $sociales_satisfaccion, $infraestructura_satisfaccion, 
+    $servicio_satisfaccion, $comunicacion_satisfaccion, $infraestructura_modernizacion, $diversificacion_servicios, $nuevos_espacios_deportivos, 
+    $servicios_futuros, $pago_compensado, $motivacion_uso_club)) {
 
-    // Mostrar preferencias y otras respuestas
-    echo "Frecuencia uso propio: " . $frecuencia_usted . "<br>";
-    echo "Frecuencia beneficiarios: " . $frecuencia_beneficiarios . "<br>";
-    echo  "<br>¿Marque la razón por la que usted está afiliado siendo 5 poco importante y 1 muy importante? <br>";
-    echo "<br> Preferencias gastronómicas: " . $gastronomicas . "<br>";
-    echo "Preferencias culturales: " . $culturales . "<br>";
-    echo "Preferencias deportivas: " . $deportivas . "<br>";
-    echo "Preferencias sociales: " . $sociales . "<br>";
-    echo "Otras preferencias: " . $text_cual . "<br>";
-    echo "Otra cual: " . $otra_cual . "<br>";
-    
-    // Datos de contacto
-    echo "Aplicación socios: " . $app_socios . "<br>";
-    echo "WhatsApp: " . $whatsapp . "<br>";
-    echo "Correo: " . $mail . "<br>";
-    
-    // Redes sociales
-    echo "Revista Somos: " . $revista_somos . "<br>";
-    echo "Instagram: " . $instagram . "<br>";
-    echo "Facebook: " . $facebook . "<br>";
-    
-    // Satisfacción general
-    echo "Satisfacción habladores: " . $habladores . "<br>";
-    echo "Mensaje de texto: " . $mensaje_texto . "<br>";
-    echo "Satisfacción general: " . $satisfaccion_general . "<br>";
-    
-    // Satisfacción por áreas
-    echo "Satisfacción deportiva: " . $deportivas_satisfaccion . "<br>";
-    echo "Satisfacción gastronómica: " . $gastronomicas_satisfaccion . "<br>";
-    echo "Satisfacción cultural: " . $culturales_satisfaccion . "<br>";
-    echo "Satisfacción social: " . $sociales_satisfaccion . "<br>";
-    echo "Satisfacción infraestructura: " . $infraestructura_satisfaccion . "<br>";
-    echo "Satisfacción servicio: " . $servicio_satisfaccion . "<br>";
-    echo "Satisfacción comunicación: " . $comunicacion_satisfaccion . "<br>";
-    echo "Satisfacción modernización infraestructura: " . $infraestructura_modernizacion . "<br>";
-    echo "Diversificación de servicios: " . $diversificacion_servicios . "<br>";
-    echo "Nuevos espacios deportivos: " . $nuevos_espacios_deportivos . "<br>";
-    echo "Servicios futuros: " . $servicios_futuros[0] . "<br>";
-    
-    // Información de pago
-    echo "Pago compensado: " . $pago_compensado . "<br>";
-    
-    // Motivación de uso del club
-    echo "Motivación para el uso del club: " . $motivacion_uso_club . "<br>";
+    // Código de manejo de error o validación
+    echo "Algunos campos no han sido definidos correctamente.";
+    }
 
 
     }else {
